347a9ab01cd5b5a49c67b8aa1df8d04df63904bf
DESKTOP-VJPAMSR\Emmilio <esego001@ucr.edu>
2017-03-04 19:37:38 -0800

    final commit

---
+++ Drifting/Assets/GrassRoadRace/Script/Girl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Girl : MonoBehaviour {
+
+    public float direction = 1f;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localRotation = Quaternion.Euler(0, 0, 0);
+        float moveAmount = Time.smoothDeltaTime * 1 * direction;
+        transform.Translate(moveAmount, 0f, 0f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "GameController") {
+            direction *= -1f;
+        }
+    }
+}
